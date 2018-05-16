9a17bb8e68fb09fc503efa33d4319aa7434ff60e
Edilon Andrade <edilonandrade@gmail.com>
2018-05-16 00:03:41 -0300

    Adicionando fonte do exemplo de Leitura.

---
+++ Fontes/ListaLeitura/ListaLeitura.App/Program.cs
@@ -0,0 +1,24 @@
+﻿using Ler.ListaLeitura.App.Negocio;
+using Ler.ListaLeitura.App.Repositorio;
+using System;
+
+namespace Ler.ListaLeitura.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var _repo = new LivroRepositorioCSV();
+
+            ImprimeLista(_repo.ParaLer);
+            ImprimeLista(_repo.Lendo);
+            ImprimeLista(_repo.Lidos);
+            Console.ReadLine();
+        }
+
+        static void ImprimeLista(ListaDeLeitura lista)
+        {
+            Console.WriteLine(lista);
+        }
+    }
+}
---
+++ Fontes/ListaLeitura/ListaLeitura.App/Repositorio/ILivroRepositorio.cs
@@ -0,0 +1,14 @@
+﻿using Ler.ListaLeitura.App.Negocio;
+using System.Collections.Generic;
+
+namespace Ler.ListaLeitura.App.Repositorio
+{
+    interface ILivroRepositorio
+    {
+        ListaDeLeitura ParaLer { get; }
+        ListaDeLeitura Lendo { get; }
+        ListaDeLeitura Lidos { get; }
+        IEnumerable<Livro> Todos { get; }
+        void Incluir(Livro livro);
+    }
+}
