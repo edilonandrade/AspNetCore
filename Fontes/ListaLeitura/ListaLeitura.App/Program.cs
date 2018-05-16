7d6400ad8dbbd0dfde354b51a39e926d524b81b0
Edilon Andrade <edilonandrade@gmail.com>
2018-05-16 19:06:23 -0300

    Adcionando comentários ao código fonte na primeira parte.

--- Fontes/ListaLeitura/ListaLeitura.App/Program.cs
+++ Fontes/ListaLeitura/ListaLeitura.App/Program.cs
@@ -10,12 +10,16 @@ namespace Ler.ListaLeitura.App
         static void Main(string[] args)
         {
             var _repo = new LivroRepositorioCSV();
+            /*Uma das opções será o servidor web adotado pela sua aplicação, que no caso será o Kestrel, 
+             * que é um servidor web já implementado pela galera do AspNet.Core.*/
 
+            //Quando a construção de um objeto é complexo, isolamos essa complexidade em uma classe, seguindo um padrão famoso chamado Builder. Então usaremos um objeto da classe WebHostBuilder para criar esse objeto IWebHost pra gente. Então fazemos new WebHostBuilder.Em seguida podemos construir o host usando o método Build().Se você quiser economizar linhas de código pode fazer diretamente assim(mostrando o uso desnecessário da váriavel builder).
             IWebHost host = new WebHostBuilder()
-                .UseKestrel()
-                .UseStartup<Startup>()
+                .UseKestrel() //Para dizer que usaremos o Kestrel, chamamos um método de extensão UseKestrel(). 
+                .UseStartup<Startup>() //Basta que informemos qual é a classe durante a construção do objeto-host usando o método de extensão UseStartup<>(), passando por generics a classe a ser utilizada. Por convenção, o nome da classe é Startup, mas você pode escolher o nome que achar melhor.
                 .Build();
 
+            //Por fim, subimos o servidor usando o método Run().
             host.Run();
 
             //ImprimeLista(_repo.ParaLer);
--- Fontes/ListaLeitura/ListaLeitura.App/Startup.cs
+++ Fontes/ListaLeitura/ListaLeitura.App/Startup.cs
@@ -7,15 +7,27 @@ namespace Ler.ListaLeitura.App
 {
     public class Startup
     {
+        /*
+         O tipo que é utilizado para definir o fluxo requisição-resposta da aplicação é a interface IApplicationBuilder.
+         Então para eu configurar um request pipeline na minha aplicação eu tenho que receber como argumento de 
+         entrada um objeto do tipo IApplicationBuilder.
+        */
         public void Configure(IApplicationBuilder app) {
-            //IApplicationBuilder: Interface para construir todo o pipeline
-            //de requisição para aplicação
-            app.Run(LivrosParaLer);
-        }
+            /*Esse tipo identifica todos os métodos que possuam como retorno um objeto 
+            do tipo Task(usado para trabalhar com paralelismo) e aceita como argumento de entrada um objeto 
+            do tipo HttpContext. */
 
+           app.Run(LivrosParaLer);
+        }
+        /*
+         No ASP.NET Core toda informação referente às requisições está representada na classe HttpContext
+         */
         public Task LivrosParaLer(HttpContext context)
         {            
             var _repo = new LivroRepositorioCSV();
+            /* para escrever uma resposta para a requisição que chegar ao servidor, 
+             * usamos o método WriteAsync() na propriedade Response             
+              */
             return context.Response.WriteAsync(_repo.ParaLer.ToString());            
         }
     }
